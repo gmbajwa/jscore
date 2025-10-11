34fc8905cdec656bbde2762ff54440e9aca62851
Mustafa Ibraheemi <tekhbox@gmail.com>
2025-10-11 20:30:48 +0500

    Ternary Operator Implimentation

---
+++ 15.js
@@ -0,0 +1,45 @@
+'use strict'
+/**
+ * and and or operator
+ * Some time we need to check more than 1 conditions
+ */
+console.log(`/**
+ * and and or operator used to check more than one condition
+ */`)
+console.log(`
+    
+    
+    `)
+let userAge = 33
+let userGender = 'Female'
+let userCanFly = (userAge >= 22 || userGender === 'Male') ? 'flay': 'wait please'
+console.log(`Hy, Respected User !!!, ${userCanFly} now`)
+console.log(`
+    
+    
+    `)    
+/**
+ * Errors In JS
+ * 
+ * 1- type of 'NON' is an Object
+ * 
+ * 2- 'use stict' mode is not working if we use it
+ * in the middle or in the bottom of file
+ * with out keyword and 'use strict' mode if we
+ * 
+ * 3- declare any veriable with out any keyword
+ * its not possible  
+ */
+
+console.log(`/**
+* Errors In JS
+* 
+* 1- type of 'NON' is an Object
+* 
+* 2- 'use stict' mode is not working if we use it
+* in the middle or in the bottom of file
+* with out keyword and 'use strict' mode if we
+* 
+* 3- declare any veriable with out any keyword
+* its not possible  
+*/`);
