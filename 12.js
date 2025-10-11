1e184ddfa2c05096e22c51b9f5f8410ccda03ee4
Mustafa Ibraheemi <tekhbox@gmail.com>
2025-10-11 14:58:38 +0500

    0, empty string, null, undefined, false are falsy values

---
+++ 12.js
@@ -0,0 +1,46 @@
+'use strict'
+/**
+ * Boolean data type
+ */
+console.log(`/**
+ * Boolean data types
+ */`)
+console.log(`
+    
+    
+    `)
+let num1 = 7
+let num2 = 7
+console.log(`num1 not equal to num2 :`, num1 !== num2)
+let num3 = 8
+let num4 = "8"
+console.log(`num3 != num4 :`, num3 !== num4)
+console.log(`
+    
+    
+    `)    
+/**
+ * Errors In JS
+ * 
+ * 1- type of 'NON' is an Object
+ * 
+ * 2- 'use stict' mode is not working if we use it
+ * in the middle or in the bottom of file
+ * with out keyword and 'use strict' mode if we
+ * 
+ * 3- declare any veriable with out any keyword
+ * its not possible  
+ */
+
+console.log(`/**
+* Errors In JS
+* 
+* 1- type of 'NON' is an Object
+* 
+* 2- 'use stict' mode is not working if we use it
+* in the middle or in the bottom of file
+* with out keyword and 'use strict' mode if we
+* 
+* 3- declare any veriable with out any keyword
+* its not possible  
+*/`);
---
+++ 13.js
@@ -0,0 +1,51 @@
+'use strict'
+
+
+
+/**
+ * Truthy and Fasly Values in JS
+ */
+console.log(`/**
+ * Truthy and Falsy Values in JS Basic Understanding
+ */`)
+console.log(`
+    
+    
+    `)
+    
+    let truthyFalsy = ``
+    if(truthyFalsy){
+        console.log(`truty`)
+    }else{
+        console.log(`false`)
+    }
+
+console.log(`
+    
+    
+    `)    
+/**
+ * Errors In JS
+ * 
+ * 1- type of 'NON' is an Object
+ * 
+ * 2- 'use stict' mode is not working if we use it
+ * in the middle or in the bottom of file
+ * with out keyword and 'use strict' mode if we
+ * 
+ * 3- declare any veriable with out any keyword
+ * its not possible  
+ */
+
+console.log(`/**
+* Errors In JS
+* 
+* 1- type of 'NON' is an Object
+* 
+* 2- 'use stict' mode is not working if we use it
+* in the middle or in the bottom of file
+* with out keyword and 'use strict' mode if we
+* 
+* 3- declare any veriable with out any keyword
+* its not possible  
+*/`);
