08beb8e6436301580b46c5ce8cde921eac2ba88a
Mustafa Ibraheemi <tekhbox@gmail.com>
2025-10-14 11:36:55 +0500

    switch and switch with break

---
+++ 19.js
@@ -0,0 +1,111 @@
+'use strict'
+/**
+ * Switch Statement
+ * set the day value and print the 
+ * set the day value and print the ay name from week days
+ */
+console.log(`/**
+ * 
+ * Switch implimentation using the week days
+ * 
+ */`)
+
+console.log(`
+    
+    
+    `)
+
+     let currentdAy = 2
+
+//  without break statement in each case,
+// once the any case get valid woithout
+// break statement in each case, all next
+// cases will also get executed like following
+
+ switch(currentdAy){
+    case 1:
+        console.log(`Its Monday`)
+    case 2:
+        console.log(`Its Tuesday`)
+    case 3:
+        console.log(`Its Wednesday`)
+    case 4:
+        console.log(`Its Thursday`)
+    case 5:
+        console.log(`Its Friday`)
+    case 6:
+        console.log(`Its Saturday`)
+    case 7:
+        console.log(`Its Sunday`)
+    default:
+        console.log(`Its not a valid day`)
+ }
+
+ console.log(`
+    
+    
+    `)    
+
+
+ /**
+  * now useing break statement in switch
+  * 
+  */
+
+ let day = "Monday"
+ switch(day){
+    case "Monday":
+        console.log(`Its Monday`)
+        break
+    case "Tuesday":
+        console.log(`Its Tuesday`)
+        break
+    case "Wednesday":
+        console.log(`Its Wednesday`)
+        break
+    case "Thursday":
+        console.log(`Its Thursday`)
+        break
+    case "Friday":
+        console.log(`Its Friday`)
+        break
+    case "Saturday":
+        console.log(`Its Satruday`)
+        break
+    case "Sunday":
+        console.log(`Its Sunday`)
+        break
+    default:
+        console.log(`Its not a valid day`)
+ }
+
+
+console.log(`
+    
+    
+    `)    
+/**
+ * Errors In JS
+ * 
+ * 1- type of 'NON' is an Object
+ * 
+ * 2- 'use stict' mode is not working if we use it
+ * in the middle or in the bottom of file
+ * with out keyword and 'use strict' mode if we
+ * 
+ * 3- declare any veriable with out any keyword
+ * its not possible  
+ */
+
+console.log(`/**
+* Errors In JS
+* 
+* 1- type of 'NON' is an Object
+* 
+* 2- 'use stict' mode is not working if we use it
+* in the middle or in the bottom of file
+* with out keyword and 'use strict' mode if we
+* 
+* 3- declare any veriable with out any keyword
+* its not possible  
+*/`);
