f8f3ccc3b9dc7d7cfc2b3ae5ae5955eb236cf8e9
Mustafa Ibraheemi <tekhbox@gmail.com>
2025-10-15 10:17:43 +0500

    While Loop example, Sum of N Natural Numbers
    For Loop Construct

---
+++ 21.js
@@ -0,0 +1,93 @@
+'use strict'
+/**
+ * Sum of first n natural numbers using While Loop
+ */
+console.log(`/**
+ * Sum of first n natural numbers using While Loop
+ */`)
+console.log(`
+    
+    
+    `)
+let num = 0
+let sum = 0
+
+sum = sum + num 
+num++
+
+sum = sum + num 
+num++
+
+sum = sum + num 
+num++
+
+sum = sum + num 
+num++
+
+sum = sum + num 
+num++
+
+sum = sum + num 
+num++
+
+sum = sum + num 
+num++
+
+sum = sum + num 
+num++
+
+sum = sum + num 
+num++
+
+sum = sum + num 
+num++
+
+console.log(`Sum of first ${num} natural numbers is : ${sum}`)
+
+
+
+// Some of first N Natural Numbers using Mathametical Formula (n*(n+1))/2
+let sum3 = 0
+let limit = 9999999999
+sum3 = (limit*(limit + 1))/2
+console.log(`Sum of first ${limit} natural numbers is : ${sum3}`)
+
+// Some of first N Natural Numbers using While Loop
+let sum2 = 0
+let counter = 0
+while(counter <= 9){
+    sum2 = sum2 + counter
+    counter++
+}
+
+console.log(`Sum of first ${counter} natural numbers is : ${sum2}`)
+
+console.log(`
+    
+    
+    `)    
+/**
+ * Errors In JS
+ * 
+ * 1- type of 'NON' is an Object
+ * 
+ * 2- 'use stict' mode is not working if we use it
+ * in the middle or in the bottom of file
+ * with out keyword and 'use strict' mode if we
+ * 
+ * 3- declare any veriable with out any keyword
+ * its not possible  
+ */
+
+console.log(`/**
+* Errors In JS
+* 
+* 1- type of 'NON' is an Object
+* 
+* 2- 'use stict' mode is not working if we use it
+* in the middle or in the bottom of file
+* with out keyword and 'use strict' mode if we
+* 
+* 3- declare any veriable with out any keyword
+* its not possible  
+*/`);
---
+++ 22.js
@@ -0,0 +1,68 @@
+'use strict'
+/**
+ * For Loop
+ */
+console.log(`/**
+ * For Loop
+    * two implimentation
+    * Iterator with in bracket and
+    * before the For Loop Construct
+    * 
+ */`)
+console.log(`
+    
+    
+    `)
+// Code is here
+
+for(let i = 0; i < 100; i++){
+    console.log(`Iterator I value is : ${i}`)
+}
+
+// we can't access i out side the for construct block
+// as let is block level not global or function level
+// do a try to and we try to access i out side the for
+// block construct
+
+// console.log(`value of i out side for block : ${i}`)
+
+// as we can see i is not defined
+
+// For Loop Construct 2nd implimentation
+let i = 0;
+for(; i < 3; i++){
+    console.log(`value of i is : ${i}`)
+}
+
+// now we can access i as its define outside the
+// loop construct
+console.log(`value of i out side the construct : ${i}`)
+console.log(`
+    
+    
+    `)    
+/**
+ * Errors In JS
+ * 
+ * 1- type of 'NON' is an Object
+ * 
+ * 2- 'use stict' mode is not working if we use it
+ * in the middle or in the bottom of file
+ * with out keyword and 'use strict' mode if we
+ * 
+ * 3- declare any veriable with out any keyword
+ * its not possible  
+ */
+
+console.log(`/**
+* Errors In JS
+* 
+* 1- type of 'NON' is an Object
+* 
+* 2- 'use stict' mode is not working if we use it
+* in the middle or in the bottom of file
+* with out keyword and 'use strict' mode if we
+* 
+* 3- declare any veriable with out any keyword
+* its not possible  
+*/`);
