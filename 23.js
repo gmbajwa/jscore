195d1583eef57c9271b48aa14712245c47f37896
Mustafa Ibrahemi <mustafaibrahemiofficial@gmail.com>
2025-10-27 17:52:53 +0500

    For Loop Construct implimentation with sum of first 10 natural numbers

---
+++ 23.js
@@ -0,0 +1,48 @@
+'use strict'
+/**
+ * Example of for Loop 
+ * Finding the sum of first 10 numbers 
+ */
+console.log(`/**
+ * 
+ */`)
+console.log(`
+    
+    
+    `)
+    let total = 0
+    let num = 100
+    for(let i = 1; i <= num; i++){
+        total += i
+    }
+    console.log(`Sum of first ${num} is ${total}`)
+
+console.log(`
+    
+    
+    `)    
+/**
+ * Errors In JS
+ * 
+ * 1- type of 'NON' is an Object
+ * 
+ * 2- 'use stict' mode is not working if we use it
+ * in the middle or in the bottom of file
+ * with out keyword and 'use strict' mode if we
+ * 
+ * 3- declare any veriable with out any keyword
+ * its not possible  
+ */
+
+console.log(`/**
+* Errors In JS
+* 
+* 1- type of 'NON' is an Object
+* 
+* 2- 'use stict' mode is not working if we use it
+* in the middle or in the bottom of file
+* with out keyword and 'use strict' mode if we
+* 
+* 3- declare any veriable with out any keyword
+* its not possible  
+*/`);
